7624eb584a07194ddba049399471c946db277ff8
Søren Mortensen <soren@sorenmortensen.com>
2017-03-15 11:05:34 +0000

    Add project with splash screen

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  SymondsStudentApp
+//
+//  Created by Søren Mortensen on 14/03/2017.
+//  Copyright © 2017 Søren Mortensen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+}
---
+++ Sources/SplashViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SplashViewController.swift
+//  SymondsStudentApp
+//
+//  Created by Søren Mortensen on 14/03/2017.
+//  Copyright © 2017 Søren Mortensen. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    
+    
+}
