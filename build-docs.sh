10834e3807308ac85d8ea7aedf0be7fc7cefb269
Søren Mortensen <soren@sorenmortensen.com>
2017-03-15 13:53:57 +0000

    auto-update docs

---
+++ build-docs.sh
@@ -0,0 +1,11 @@
+bundle install
+git submodule update --remote
+
+bundle exec jazzy \
+  --source-directory SymondsStudentApp/ \
+  --readme SymondsStudentApp/README.md \
+  --module SymondsStudentApp \
+  --min-acl private \
+  --theme apple \
+  --github_url "https://www.github.com/geosor/SymondsStudentApp" \
+  --author "Søren Mortensen, George Taylor"
