2293f98b16d3d6a428f13b25d32970a15d7d18a3
Søren Mortensen <soren@sorenmortensen.com>
2017-03-15 13:37:03 +0000

    Add jazzy

--- Sources/SplashViewController.swift
+++ Sources/SplashViewController.swift
@@ -3,7 +3,7 @@
 //  SymondsStudentApp
 //
 //  Created by Søren Mortensen on 14/03/2017.
-//  Copyright © 2017 Søren Mortensen. All rights reserved.
+//  Copyright © 2017 Søren Mortensen, George Taylor. All rights reserved.
 //
 
 import UIKit
