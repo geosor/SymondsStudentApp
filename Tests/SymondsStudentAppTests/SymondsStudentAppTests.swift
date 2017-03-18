8745a8439757b342518ffa1fdee6fb4a6ce5399a
Søren Mortensen <soren@sorenmortensen.com>
2017-03-18 12:34:53 +0000

    Fix swiftlint warnings

--- Tests/SymondsStudentAppTests/SymondsStudentAppTests.swift
+++ Tests/SymondsStudentAppTests/SymondsStudentAppTests.swift
@@ -12,11 +12,13 @@ class SymondsStudentAppTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Put setup code here. This method is called before the invocation of each test method in
+        // the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Put teardown code here. This method is called after the invocation of each test method in
+        // the class.
         super.tearDown()
     }
     
